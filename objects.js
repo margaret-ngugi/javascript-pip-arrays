6122cd46ceb8530afc4c81783e62af497a3de254
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-05-06 21:10:46 +0300

    first commit

--- objects.js
+++ objects.js
@@ -1,62 +1,58 @@
 //Q1
 //Given an array of strings, use a function to reverse all the elements in the string 
 //in ascending order and the specific elements in descending order
-
-
-
-
-
-
-
+ 
 
 //Q3
 //Given an array of objects, each object representing a person with a name and age property
 //, write a function that returns the sum of all people who are less than 18 years.
-//const people = [
+// const people = [
 //  { name: 'Alice', age: 17 },
 //  { name: 'Eunice', age: 22 },
- // { name: 'Charlie', age: 14 },
- // { name: 'Max', age: 19 },
-//];
+//  { name: 'Charlie', age: 14 },
+//  { name: 'Max', age: 19 },
+// ];
 
-const people = [
+ const people = [
       { name: 'Alice', age: 17 },
       { name: 'Eunice', age: 22 },
       { name: 'Charlie', age: 14 },
-      { name: 'Max', age: 19 },
+      { name: 'Max', age: 19 }
     ];
-
-let ages=people.map(item=>item.age>18)
-console.log({ages});
-    
-   
-    
-
-
+function peopleInformation(people){
+    return people.filter(people=>people.age<18,
+        people=>people.name)
+}
+console.log(peopleInformation(people))
 
 //Q3
 //Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
-function numArray(nums){
-    if(nums>0){
+function numArray(num){
+    if(num>0){
         console.log("num is positive");
-    }else if(nums==0){
+    }else if(num==0){
         console.log("the num is zero");
     }else{
         console.log("the num is negative");
     }
 }
-let nums=[1,2,3,4,5,6,7,8,9]
+let nums=[3]
 console.log(numArray(nums))
 
 //Given an array of objects, where each object represents an employee with an id,
 // name, and salary property, write a function that returns a new array of employee 
 //objects sorted by their salary in ascending order.
-const employee=[
-    {name:"Faith",id:"389274653",salary:15000},
-    {name:"John",id:"378291634",salary:1300},
-    {name:"Faith",id:"36728093",salary:1400}
+const details=[
+    {name:"Faith",id:1,salary:15000},
+    {name:"John",id:3,salary:1300},
+    {name:"Faith",id:2,salary:1400}
     
 
 ]
+function peopleDetails(details) {
+    const sort = details.sort((a, b) => a.salary - b.salary);
+    return sort;
+  }
+console.log(peopleInformation(details.salary));
 
 
